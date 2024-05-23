488b386b44c31ae09ad273c42edf50ec9d203018
Michael Innerberger <innerbergerm@hhmi.org>
2024-05-23 17:57:40 -0400

    Update to new version with improved installers

--- recipes/stim/build.sh
+++ recipes/stim/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin
-./install $PREFIX/bin
+INSTALL_DIR="${PREFIX}/bin"
+MVN_REPOSITORY="${PREFIX}/lib/stim"
+
+mkdir -p ${INSTALL_DIR}
+mkdir -p ${MVN_REPOSITORY}
+
+./install.sh -i ${INSTALL_DIR} -r ${MVN_REPOSITORY}
