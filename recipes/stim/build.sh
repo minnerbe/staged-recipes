ea5ae66cd0f677a6166a2ab27ac59c0f9c5f3033
Michael Innerberger <innerbergerm@hhmi.org>
2024-05-24 11:29:12 -0400

    Remove PREFIX substitution (automatically done by conda)
    See https://conda.io/projects/conda-build/en/stable/resources/make-relocatable.html

--- recipes/stim/build.sh
+++ recipes/stim/build.sh
@@ -8,6 +8,3 @@ mkdir -p ${MVN_REPOSITORY}
 
 ./install.sh -i ${INSTALL_DIR} -r ${MVN_REPOSITORY}
 
-# conda uses a dummy-prefix to build, but the path is hard-coded in the commands
-# replace by a variable that points to the correct location on the host
-find $PREFIX/bin -name st-* | xargs sed -i "s,${PREFIX},\$CONDA_PREFIX,g"
